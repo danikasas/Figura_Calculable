97095e2b41344a8148d7508efad08f8a085ebba4
danikasas <danikasas192@hotmail.com>
2013-08-12 01:34:39 -0500

    Trabajo Figura

---
+++ src/main/java/co/edu/um/Figura_Calculable/App.java
@@ -0,0 +1,13 @@
+package co.edu.um.Figura_Calculable;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
